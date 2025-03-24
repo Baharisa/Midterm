86703d46f39f018c3314f0dcefd9d539dc33382c
Mahani Bahari <mahanibahari@aol.com>
2025-03-24 00:17:14 -0400

    Fix: return plain arrays from authors and categories endpoints

--- api/authors/read.php
+++ api/authors/read.php
@@ -1,35 +1,29 @@
- 
 <?php
-include_once '../config/Database.php';
-include_once '../models/Author.php';
-include_once '../header.php';
+include_once '../../config/Database.php';
+include_once '../../models/Author.php';
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
 $database = new Database();
 $db = $database->getConnection();
-
 $author = new Author($db);
 
-$stmt = $author->read();
-$num = $stmt->rowCount();
+$result = $author->read();
+$num = $result->rowCount();
 
 if ($num > 0) {
-    $authors_arr = array();
-    $authors_arr["records"] = array();
+    $authors_arr = [];
 
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
-        $author_item = array(
-            "id" => $id,
-            "author" => $author
-        );
-
-        array_push($authors_arr["records"], $author_item);
+        $authors_arr[] = [
+            'id' => $id,
+            'author' => $author
+        ];
     }
 
-    http_response_code(200);
     echo json_encode($authors_arr);
 } else {
-    http_response_code(404);
-    echo json_encode(array("message" => "No authors found."));
+    echo json_encode([]);
 }
-?>
--- api/categories/read.php
+++ api/categories/read.php
@@ -1,35 +1,29 @@
- 
 <?php
-include_once '../config/Database.php';
-include_once '../models/Category.php';
-include_once '../header.php';
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
 
 $database = new Database();
 $db = $database->getConnection();
-
 $category = new Category($db);
 
-$stmt = $category->read();
-$num = $stmt->rowCount();
+$result = $category->read();
+$num = $result->rowCount();
 
 if ($num > 0) {
-    $categories_arr = array();
-    $categories_arr["records"] = array();
+    $categories_arr = [];
 
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
         extract($row);
-        $category_item = array(
-            "id" => $id,
-            "category" => $category
-        );
-
-        array_push($categories_arr["records"], $category_item);
+        $categories_arr[] = [
+            'id' => $id,
+            'category' => $category
+        ];
     }
 
-    http_response_code(200);
     echo json_encode($categories_arr);
 } else {
-    http_response_code(404);
-    echo json_encode(array("message" => "No categories found."));
+    echo json_encode([]);
 }
-?>
