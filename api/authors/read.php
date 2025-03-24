ec2bd434abffa60ff00bf9b95bf4dd480f9f5bc5
Mahani Bahari <mahanibahari@aol.com>
2025-03-24 00:34:20 -0400

    Fix: return plain JSON array in authors/read.php for Netlify test

--- api/authors/read.php
+++ api/authors/read.php
@@ -1,29 +1,25 @@
 <?php
-include_once '../../config/Database.php';
-include_once '../../models/Author.php';
-
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
+include_once '../../config/Database.php';
+include_once '../../models/Author.php';
+
 $database = new Database();
 $db = $database->getConnection();
+
 $author = new Author($db);
+$stmt = $author->read();
 
-$result = $author->read();
-$num = $result->rowCount();
+$authors_arr = [];
 
-if ($num > 0) {
-    $authors_arr = [];
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    extract($row);
 
-    while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
-        $authors_arr[] = [
-            'id' => $id,
-            'author' => $author
-        ];
-    }
-
-    echo json_encode($authors_arr);
-} else {
-    echo json_encode([]);
+    $authors_arr[] = [
+        'id' => $id,
+        'author' => $author
+    ];
 }
+
+echo json_encode($authors_arr);
